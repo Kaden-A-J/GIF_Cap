05e9311831e0236c448185d0f9d65f550b4367b0
Kaden-A-J <kaden.andrew.jones@gmail.com>
2024-10-22 17:10:42 -0600

    darken screen, transp selection box, switch to pyqt6

--- GIF_Cap.py
+++ GIF_Cap.py
@@ -1,10 +1,10 @@
 
-# Darken the screen
+#✅ Darken the screen
 # - Transparent black image?
 # - Apply to all screens
 
-# Let user click and drag to select bounding box 
-# - Lighten selected area
+#✅ Let user click and drag to select bounding box 
+#✅ - Lighten selected area
 
 # Capture picture from bounded area
 
@@ -15,3 +15,87 @@
 # Stitch saved images together into a GIF
 # - Save GIF to folder
 # - Save GIF to clipboard
+
+from PyQt6.QtGui import QKeyEvent, QMouseEvent, QRegion
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt6.QtCore import QPointF, QRectF, Qt
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("GIF_Cap")
+        self.setGeometry(0, 0, 500, 500)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents, False)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground, False)
+
+        self.mouse_down_coords = QPointF(0, 0)
+        self.mouse_up_coords = QPointF(0, 0)
+        self.box_rect = QRectF(0, 0, 0, 0)
+
+        self.resize_box = QWidget()
+        self.resize_box.setStyleSheet("background-color:rgba(0, 0, 0, 0)")
+        self.setStyleSheet("background-color:rgba(0, 0, 0, 40)")
+
+        container = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(self.resize_box)
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+
+    def keyPressEvent(self, e: QKeyEvent | None) -> None:
+        if (e.key() == Qt.Key.Key_Escape):
+            self.close()
+
+
+    def mousePressEvent(self, e):
+        self.mouse_down_coords = e.pos()
+        mouse_down_point = self.mouse_down_coords.toPointF()
+
+        self.resize_box.setGeometry(int(mouse_down_point.x()), int(mouse_down_point.y()), 0, 0)
+        
+
+    def mouseReleaseEvent(self, e):
+        self.mouse_up_coords = e.pos()
+        
+    
+    def mouseMoveEvent(self, e: QMouseEvent | None) -> None:
+        mouse_move_point = e.pos().toPointF()
+        mouse_down_point = self.mouse_down_coords.toPointF()
+
+        # if mouse is right of where initial click is
+        if (mouse_move_point.x() - mouse_down_point.x() > 0):
+            x = self.mouse_down_coords.x()
+            w = mouse_move_point.x() - mouse_down_point.x()
+        else: # otherwise change 'initial position' and modify width to be where it originally was
+            x = mouse_move_point.x()
+            w = mouse_down_point.x() - mouse_move_point.x()
+
+        # if mouse is below of where initial click it
+        if (mouse_move_point.y() - mouse_down_point.y() > 0):
+            y = self.mouse_down_coords.y()
+            h = mouse_move_point.y() - mouse_down_point.y()
+        else: # otherwise change 'initial position' and modify height to be where it originally was
+            y = mouse_move_point.y()
+            h = mouse_down_point.y() - mouse_move_point.y()
+
+        self.box_rect = QRectF(x, y, w, h)
+
+        self.resize_box.setGeometry(self.box_rect.toRect())
+
+        mask = QRegion(self.box_rect.toRect(), QRegion.RegionType.Rectangle)
+        empty_region = QRegion(self.geometry(), QRegion.RegionType.Rectangle)
+        self.setMask(empty_region.subtracted(mask))
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.showFullScreen()
+
+app.exec()
