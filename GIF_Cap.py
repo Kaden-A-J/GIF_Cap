cc59fd44db6a32b3352d7953d985288d53951075
Kaden-A-J <kaden.andrew.jones@gmail.com>
2024-10-22 10:51:19 -0600

    setup

---
+++ GIF_Cap.py
@@ -0,0 +1,17 @@
+
+# Darken the screen
+# - Transparent black image?
+# - Apply to all screens
+
+# Let user click and drag to select bounding box 
+# - Lighten selected area
+
+# Capture picture from bounded area
+
+# Capture a picture (24?) frames a second?  (41.66 ms per frame)
+# - Hotkey to stop capturing (Esc?)
+# - Save individual images in folder? (Start with lower FPS like 3)
+
+# Stitch saved images together into a GIF
+# - Save GIF to folder
+# - Save GIF to clipboard
